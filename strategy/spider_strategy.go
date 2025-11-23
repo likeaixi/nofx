246061fddcdd4778c07afbafe9c05f540e9778ca
like <likeaixi@gmail.com>
2025-11-23 13:36:48 +0800

    更新决策时间

--- strategy/spider_strategy.go
+++ strategy/spider_strategy.go
@@ -519,7 +519,7 @@ func (s *SpiderStrategy) GetFullDecision(ctx *decision.Context) (*decision.FullD
 		fullDecision.Timestamp = time.Now()
 		fullDecision.SystemPrompt = "Spider strategy" // 保存系统prompt
 		fullDecision.UserPrompt = "Spider strategy"   // 保存输入prompt
-		fullDecision.AIRequestDurationMs = aiCallEnd - aiCallStart
+		fullDecision.AIRequestDurationMs = (aiCallEnd - aiCallStart) / 1000
 	}
 
 	log.Printf("Decision end")
